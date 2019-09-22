370d755fa47fd285165788073097edafaaf6dda3
David Araujo <davidsdearaujo@gmail.com>
2019-09-22 01:18:46 -0300

    Adicionado item no primeiro card

--- lib/src/home/card_list/card_list_widget.dart
+++ lib/src/home/card_list/card_list_widget.dart
@@ -8,102 +8,125 @@ class CardListWidget extends StatelessWidget {
     return PageView(
       scrollDirection: Axis.horizontal,
       children: <Widget>[
-        CardWidget(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Container(
-                  padding: const EdgeInsets.all(25),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Column(
+        Card1(),
+      ],
+    );
+  }
+}
+
+class Card1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CardWidget(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
                           children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                Icon(Icons.credit_card, color: Colors.black54),
-                                SizedBox(width: 15),
-                                Text(
-                                  "Cartão de crédito",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.black54,
-                                  ),
-                                ),
-                              ],
+                            Icon(Icons.credit_card, color: Colors.black54),
+                            SizedBox(width: 15),
+                            Text(
+                              "Cartão de crédito",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black54,
+                              ),
                             ),
-                            Expanded(
-                              child: MediaQuery(
-                                data: MediaQuery.of(context).copyWith(
-                                  textScaleFactor: MediaQuery.of(context).size.width * 0.003,
-                                ),
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Container(
-                                    width: double.infinity,
-                                    child: Column(
+                          ],
+                        ),
+                        Expanded(
+                          child: MediaQuery(
+                            data: MediaQuery.of(context).copyWith(
+                              textScaleFactor:
+                                  MediaQuery.of(context).size.width * 0.003,
+                            ),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                width: double.infinity,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "FATURA ATUAL",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFF00BDC6),
+                                      ),
+                                    ),
+                                    Text(
+                                      "R\$ 1.828,24",
+                                      style: TextStyle(
+                                        color: Color(0xFF00BDC6),
+                                        fontSize: 30,
+                                      ),
+                                    ),
+                                    Row(
                                       mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: <Widget>[
+                                        Text("Limite disponível "),
                                         Text(
-                                          "FATURA ATUAL",
+                                          "R\$ 406,34",
                                           style: TextStyle(
                                             fontWeight: FontWeight.bold,
-                                            color: Color(0xFF00BDC6),
+                                            color: Color(0xFF9CD236),
                                           ),
-                                        ),
-                                        Text(
-                                          "R\$ 1.828,24",
-                                          style: TextStyle(
-                                            color: Color(0xFF00BDC6),
-                                            fontSize: 30,
-                                          ),
-                                        ),
-                                        Row(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: <Widget>[
-                                            Text("Limite disponível "),
-                                            Text(
-                                              "R\$ 406,34",
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: Color(0xFF9CD236),
-                                              ),
-                                            ),
-                                          ],
                                         ),
                                       ],
                                     ),
-                                  ),
+                                  ],
                                 ),
                               ),
-                            )
-                          ],
-                        ),
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Container(
-                          width: 8,
-                          color: Colors.black,
-                          child: Column(),
-                        ),
-                      )
-                    ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      width: 8,
+                      color: Colors.black,
+                      child: Column(),
+                    ),
+                  )
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Theme.of(context).dividerColor,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ],
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Theme.of(context).dividerColor,
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.settings),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                        "Compra mais recente em Netflix.Com no valor de R\$ 45,90 ontem",
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.keyboard_arrow_right),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
